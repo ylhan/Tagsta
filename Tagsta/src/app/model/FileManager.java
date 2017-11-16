187adea46e0e02a2721b7919b110da161e7263c6
Amritpal <amritpal.aujla@utoronto.ca>
2017-11-16 03:14:33 -0500

    Added a saveFiles method that will save the config file and the imageManagers. Implemented the storeImageManagers and loadImageManagers methods to allow the serialization of imageManagers. Partly implemented the loadConfig method and created and implemented a helper method for it, getConfigFile.

--- Tagsta/src/app/model/FileManager.java
+++ Tagsta/src/app/model/FileManager.java
@@ -1,34 +1,79 @@
 package app.model;
+import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class FileManager {
     private Path configPath;
 
-    public FileManager() {
+    public FileManager() throws IOException{
+        this.loadConfig();
+    }
+
+    public void saveFiles(ArrayList<ImageManager> imageManagers){
+        this.storeConfig();
+        this.storeImageManagers(imageManagers);
+    }
+
+    private Path getConfigFile() throws IOException{
+        Path configPath = Paths.get("config.xml");
+        File file = configPath.toFile();
+        if (!file.exists()) {
+            System.out.println("file does not exist");
+            this.storeConfig();
+        }
+        return configPath;
+    }
+
+    private void storeImageManagers(ArrayList<ImageManager> imageManagers) {
+        try{
+            OutputStream file = new FileOutputStream("imageManagers");
+            OutputStream buffer = new BufferedOutputStream(file);
+            ObjectOutput output = new ObjectOutputStream(buffer);
+
+            output.writeObject(imageManagers);
+            output.close();
+        }
+        catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
+
+    public ArrayList<ImageManager> loadImageManagers() {
+        ArrayList<ImageManager> imageManagers;
+        try {
+            InputStream file = new FileInputStream("imageManagers");
+            InputStream buffer = new BufferedInputStream(file);
+            ObjectInput input = new ObjectInputStream(buffer);
+
+            imageManagers = (ArrayList<ImageManager>) input.readObject();
+            input.close();
+        } catch (IOException ex) {
+            System.out.println("io exception");
+            imageManagers = new ArrayList<>();
+            this.storeImageManagers(imageManagers);
+            return imageManagers;
+        }
+        catch (ClassNotFoundException ex){
+            System.out.println("classpath is broken");
+            imageManagers = new ArrayList<>();
+            this.storeImageManagers(imageManagers);
+            return imageManagers;
+        }
+        return imageManagers;
+    }
+
+    private void storeConfig() {
 
     }
 
-    public void getFilesFromDirectory(Path path) {
-
+    private void loadConfig() throws IOException{
+        this.configPath = this.getConfigFile();
     }
 
-    public void storeImageManagers() {
-
-    }
-
-    public void loadImageManagers() {
-
-    }
-
-    public void storeConfig() {
-
-    }
-
-    public void loadConfig(){
-
-    }
-
-    public void moveImage() {
+    public void moveImage(Path currentPath, Path newPath) {
 
     }
 }
