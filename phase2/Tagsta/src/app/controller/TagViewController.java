990f90fbbd91e7278b2e75b590d9169b1c921159
Samin Khan <samin.khan995@gmail.com>
2017-11-28 19:17:51 -0500

    Created ListView in TagView for independent tag list. Added methods for updating tag list from all list of tags in directory, adding tags to the independent tag list, and for selecting tags from the independent tag list to add to the current image.
    Note: The list of tags has not properly implemented parsing for tags in the image names of files in a root directory as of yet.

--- phase2/Tagsta/src/app/controller/TagViewController.java
+++ phase2/Tagsta/src/app/controller/TagViewController.java
@@ -2,13 +2,18 @@ package app.controller;
 
 import app.Tagsta;
 import app.model.ImageManager;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TreeItem;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
@@ -17,6 +22,7 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
 
 /** Controller for the tag view at the bottom right of the overview */
 public class TagViewController {
@@ -25,10 +31,38 @@ public class TagViewController {
   @FXML private FlowPane tagView;
   @FXML private TextField addTagTextField;
 
+  @FXML private ListView<String> tagListView;
+
+
   private Stage revertWindow;
   private Tagsta main;
   private ImageManager imageManager;
   private DirectoryViewController directoryViewController;
+
+  public void updateTagList() {
+    ObservableList<String> uniqueTagList = FXCollections.observableArrayList(
+            new HashSet<>(main.getTagManager().getTagsList()));
+    tagListView.setItems(uniqueTagList.sorted());
+  }
+
+  private void addTagList(String tag) {
+    if (!tagListView.getItems().contains(tag)) {
+      ObservableList<String> tempList = tagListView.getItems();
+      tempList.add(tag);
+      tagListView.setItems(tempList.sorted());
+    }
+  }
+
+  @FXML
+  private void handleTagListClick(MouseEvent event) {
+    if (event.getClickCount() == 2) {
+      String tagItem = tagListView.getSelectionModel().getSelectedItem();
+      tagView.getChildren().add(createTag(tagItem));
+      imageManager.addTag(tagItem);
+      // updateFileName(new TreeItem<>(imageManager.getFile()));
+      main.getTagManager().saveProgram();
+    }
+  }
 
   /**
    * Loads new tags into the tag view given the image manager of the image
