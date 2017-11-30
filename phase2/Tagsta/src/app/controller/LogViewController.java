257aed3f93b9533109560a3a28b726a0b00c42d3
Yilun <allenylhan@gmail.com>
2017-11-30 18:49:54 -0500

    Added copy to clipboard when double clicking log item

--- phase2/Tagsta/src/app/controller/LogViewController.java
+++ phase2/Tagsta/src/app/controller/LogViewController.java
@@ -3,14 +3,15 @@ package app.controller;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 
-/**
- * Controller class for the log view window
- */
+/** Controller class for the log view window */
 public class LogViewController {
 
-  @FXML
-  private ListView<String> log;
+  @FXML private ListView<String> log;
 
   /**
    * Set the logs to be displayed
@@ -23,11 +24,20 @@ public class LogViewController {
   }
 
   /**
-   * Initialize the log items to be unselectable (only viewable)
+   * Handles double-clicking of a log item. When a log item is double clicked it is copied.
+   *
+   * @param event the user's click
    */
   @FXML
-  private void initialize() {
-    log.setMouseTransparent(true);
-    log.setFocusTraversable(false);
+  private void handleClick(MouseEvent event) {
+    if (event.getButton().equals(MouseButton.PRIMARY) && event.getClickCount() == 2) {
+      // Get the user's selected item and then place it in the clipboard
+      String selectedLogItem = log.getSelectionModel().getSelectedItems().get(0);
+      if (selectedLogItem != null) {
+        ClipboardContent content = new ClipboardContent();
+        content.putString(selectedLogItem);
+        Clipboard.getSystemClipboard().setContent(content);
+      }
+    }
   }
 }
