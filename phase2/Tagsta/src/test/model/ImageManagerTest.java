21d978f44c019a952f63c390005142fc92156fb0
Yilun <allenylhan@gmail.com>
2017-11-30 01:09:32 -0500

    Created JUnit test for ImageManager

---
+++ phase2/Tagsta/src/test/model/ImageManagerTest.java
@@ -0,0 +1,12 @@
+import app.model.ImageManager;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class ImageManagerTest {
+
+    @Test
+    public void ImageManagerTagLoadTest() {
+    }
+}
